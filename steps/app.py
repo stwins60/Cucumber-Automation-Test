ad2c44ce2ac8f87a7c69c62b42551994fd1f22d7
Idris Fagbemi <idrisniyi94@gmail.com>
2022-03-12 01:45:28 -0600

    update app.py

--- steps/app.py
+++ steps/app.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from behave import *
+from chromedriver_py import binary_path
 import json, requests
 import sys, time, random
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
 sys.setrecursionlimit(1500)
 
 RAND = random.randint(100, 1000)
@@ -10,7 +17,8 @@ RAND = random.randint(100, 1000)
 # # @when
 @given('I have a valid API key and I go to the API endpoint')
 def get_driver(context):
-    context.driver = webdriver.Chrome(r'C:\chromedriver.exe')
+#     context.driver = webdriver.Chrome(r'C:\chromedriver.exe')
+    context.driver = webdriver.Chrome(binary_path, chrome_options=options)
     context.driver.maximize_window()
     context.driver.implicitly_wait(10)
     context.driver.get(f'https://api.themoviedb.org/3/movie/{RAND}?api_key=37b46b26998e792429f57babd3432089&language=en-US')
@@ -19,7 +27,8 @@ def get_driver(context):
 
 @then("I should get the latest movie")
 def api_test(context):
-    context.driver = webdriver.Chrome(r'C:\chromedriver.exe')
+#     context.driver = webdriver.Chrome(r'C:\chromedriver.exe')
+    context.driver = webdriver.Chrome(binary_path, chrome_options=options)
     context.driver.get('https://api.themoviedb.org/3/movie/latest?api_key=37b46b26998e792429f57babd3432089&language=en-US')
     response = context.driver.page_source
     time.sleep(5)
@@ -41,7 +50,8 @@ def api_test_3(context):
 @then("I should get the movie rating")
 def api_test_4(context):
     movie_id = RAND
-    context.driver = webdriver.Chrome(r'C:\chromedriver.exe')
+#     context.driver = webdriver.Chrome(r'C:\chromedriver.exe')
+    context.driver = webdriver.Chrome(binary_path, chrome_options=options)
     context.driver.implicitly_wait(10)
     context.response = context.driver.get(f'https://api.themoviedb.org/3/movie/{movie_id}/rating?\
     api_key=37b46b26998e792429f57babd3432089&language=en-US')
